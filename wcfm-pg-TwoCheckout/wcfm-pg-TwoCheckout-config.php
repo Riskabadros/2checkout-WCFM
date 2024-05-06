eb97b6d386e176c5495778f5995a1b1206764cec
Riskabadros <141786801+Riskabadros@users.noreply.github.com>
2024-05-06 16:06:54 +0100

    Update wcfm-pg-TwoCheckout-config.php

--- wcfm-pg-TwoCheckout/wcfm-pg-TwoCheckout-config.php
+++ wcfm-pg-TwoCheckout/wcfm-pg-TwoCheckout-config.php
@@ -4,12 +4,13 @@ define('WCFMpgmp_TOKEN', 'wcfm-twoco');
 
 define('WCFMpgmp_TEXT_DOMAIN', 'wcfm-twoco');
 
-define('WCFMpgmp_VERSION', '1.0.0');
+define('WCFMpgmp_VERSION', '1.0.1');
 
-define('WCFMpgmp_SERVER_URL', 'https://your-server-url.com');
+define('WCFMpgmp_SERVER_URL', 'https://github.com/Riskabadros/2checkout-WCFMcom');
 
 define('WCFMpgmp_GATEWAY', 'twocheckout');
 
 define('WCFMpgmp_GATEWAY_LABEL', 'TwoCheckout');
 
 ?>
+
